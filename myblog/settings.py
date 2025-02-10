f74ef47e0a23696a297c63bc4acbfdff4400ea3a
venumadhav <vm20010524@gmail.com>
2025-02-10 21:55:55 +0530

    change made in .env and gitignore files

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+# load the .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j5(s@6yvfj)00#@_cj-q4l_+60rr2&#@+aord4k@dm9cw%pm-j'
-
+SECRET_KEY =  os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
