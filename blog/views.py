0b50bad1ba0438e671dc10365022ea9118a5cc70
venumadhav <vm20010524@gmail.com>
2025-02-10 21:47:09 +0530

    added the templates and static folders and fetched data from models

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def home(request):
+    # fetch all data
+    post_data = Post.objects.all()
+    return render(request, "home.html", {"posts": post_data})
